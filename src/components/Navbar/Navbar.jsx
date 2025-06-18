42254281ee546c77acba3757e8b4da4a95476e2e
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-06-18 21:45:16 +0200

    Make the navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,48 @@
 import { Link } from "react-router-dom";
 import styles from "./navbar.module.css";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleClose = () => setMenuOpen(false);
+
   return (
     <header className={styles.header}>
-      <ul className={styles.navBar}>
-        <li className={styles.logo}>
-          <Link to="/">Shopping Cart</Link>
-        </li>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/items">Items</Link>
-        </li>
-        <li>
-          <Link to="/cart">Cart</Link>
-        </li>
-      </ul>
+      <div className={styles.navContainer}>
+        <div className={styles.mobile}>
+          <Link to="/" className={styles.logoMobile} onClick={handleClose}>
+            Shopping Cart
+          </Link>
+          <button
+            className={styles.menuButton}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            ☰
+          </button>
+        </div>
+
+        <ul className={`${styles.navBar} ${menuOpen ? styles.open : ""}`}>
+          <li className={styles.logo}>
+            <Link to="/">Shopping Cart</Link>
+          </li>
+          <li>
+            <Link to="/" onClick={handleClose}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/items" onClick={handleClose}>
+              Items
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart" onClick={handleClose}>
+              Cart
+            </Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
