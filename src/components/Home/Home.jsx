7972ffac3de43544d1542dfa21edd5a2b588be2c
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-06-19 13:17:01 +0200

    Create a home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+import styles from "./home.module.css";
+
 export default function Home() {
-  return;
+  return (
+    <div className={styles.homeWrapper}>
+      <div className={styles.home}>
+        <h1>Welcome to Shopping Cart!</h1>
+        <p>
+          Discover a wide range of quality products at unbeatable prices. From
+          everyday essentials to unique finds, we've got something for everyone.
+          Browse our items, add your favorites to the cart, and enjoy a smooth,
+          simple shopping experience.
+        </p>
+        <Link to="items">
+          <button>Go Shopping! 🛒</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
